1ee8aee4574a141ca9984d9d97e2f689e20a3fe1
alloza95 <naldusbros@gmail.com>
2020-05-31 18:33:27 +0200

    Pass arguments to initState

--- lib/src/screens/route_line_screen.dart
+++ lib/src/screens/route_line_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-//TODO: Passar dades a [route_line_screen] des de [lines_screen].
+// Passar dades a [route_line_screen] des de [lines_screen].
 
 class RouteLine extends StatefulWidget {
   // 1. Definir les dades que necessito passar i el constructor.
@@ -13,13 +13,26 @@ class RouteLine extends StatefulWidget {
 }
 
 class _RouteLineState extends State<RouteLine> {
+  RouteLine args;
+  String code = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // 2. Extreure les dades de la propietat settings del ModalRoute actual i
+    // convertir-lo en un objecte RouteLine.
+    // La classe Future ens permet accedir al context.
+    Future.delayed(Duration.zero, (){
+      setState(() {
+        args = ModalRoute.of(context).settings.arguments;
+        code = args.codeLine;
+      });
+      print('Estic al initState. Code: ' + args.codeLine);
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
-    // 2. Extreure les dades de la propietat settings del ModalRoute actual i
-    // convertir-lo en un objecte RouteLine.
-    final RouteLine args = ModalRoute.of(context).settings.arguments;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Pantalla ruta'),
@@ -27,7 +40,7 @@ class _RouteLineState extends State<RouteLine> {
       body: Center(
         // 3. Mostrar les dades extretes en el pas anterior.
         // (El pas 4 està a [line_item])
-        child: Text(args.codeLine),
+        child: Text(code),
       ),
     );
   }
